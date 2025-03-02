85fc97d38b575d0058e512ade4f0ba7332f4c186
practiseSpoo <spoorthyhrgowda034@gmail.com>
2025-03-02 09:27:41 +0530

    '2025another'

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,6 +1,6 @@
 public class Demo1 {
 
     public static void  main (Strings[] args){
-        System.out.println("My name is spoorthy and I like to play ball");
+        System.out.println("My name is spoorthy");
     }
 }
